4817d56971cb9dcd26b5d6fa7919060063c92d88
weihaibo <weihaibo@bogon>
2019-05-17 10:15:35 +0800

    change get info

--- src/main/java/com/simon/boot/word/controller/WordUserController.java
+++ src/main/java/com/simon/boot/word/controller/WordUserController.java
@@ -43,8 +43,8 @@ public class WordUserController {
         return service.login(vo);
     }
 
-    @GetMapping("info/{token}")
-    public ReturnValue info(@PathVariable String token){
+    @PostMapping("info")
+    public ReturnValue info(String token){
         return service.info(token);
     }
 
