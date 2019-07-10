1f370bc6b27a55c9b691bcc580a743a9d218a67a
simon <554040883@qq.com>
2019-07-10 22:33:23 +0800

    add auth

--- src/test/java/com/simon/boot/word/WordApplicationTests.java
+++ src/test/java/com/simon/boot/word/WordApplicationTests.java
@@ -1,6 +1,5 @@
 package com.simon.boot.word;
 
-import com.alibaba.druid.filter.config.ConfigTools;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +12,7 @@ public class WordApplicationTests {
 	@Test
 	public void contextLoads() {
 		try {
-			String encrypt = ConfigTools.encrypt("MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKV2SMCZ2c47iBsYnP3uiYTKiYXZpJdqffx3g0kLPkY/YBSbyeYbWG5JRXxscz4oZTR+RZksff2FBwGGxEyafusCAwEAAQ==", "7589014bnAa");
-			System.out.println(encrypt);
+			//TODO
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
